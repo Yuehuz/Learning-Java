c4d03fb9b580e00f6f171a0ce58d5c7f5038fac1
Yue Hu Zheng <yuehuz0527@gmail.com>
2024-11-17 23:33:56 -0500

    added main file and readme

--- Repository/Java 101/java101/Main.java
+++ Repository/Java 101/java101/Main.java
@@ -1,70 +1,8 @@
 package java101;
 
-public class Main {
-
-    static int countOccurrences(int[] arr, int n) {
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == n) {
-                count++;
-            }
-        }
-        return count;
+public class Main{
+    public static void main(String[] args) {
+        int [] arr = {0, 0, 0, 0};
+        System.out.print(PracticeProblems.countOccurrences(0, arr));
     }
-
-    static int[] reverseArray(int[] arr) {
-        int[] reversed = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            reversed[i] = arr[arr.length - 1 - i];
-        }
-        return reversed;
-    }
-
-    static double sumGrid(double[][] grid) {
-        double sum = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                sum += grid[i][j];
-            }
-        }
-        return sum;
-    }
-
-    static int fib(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        int a = 0, b = 1, result = 0;
-        for (int i = 2; i <= n; i++) {
-            result = a + b;
-            a = b;
-            b = result;
-        }
-        return result;
-    }public static void main(String[] args) {
-        
-        int[] arr = {1, 2, 3, 2, 2, 5};
-        int result = countOccurrences(arr, 2);
-        System.out.println("Below are the results for each of the functions above:");
-        System.out.println("Number 2 appears " + result + " times in the array.");
-
-        int[] reversedArray = reverseArray(arr);
-        System.out.print("Reversed array: ");
-        for (int num : reversedArray) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
-
-        double[][] grid = {
-            {1.2, 2.3},
-            {3.4, 4.5}
-        };
-        System.out.println("The sum of all elements in the grid is: " + sumGrid(grid));
-
-        int n = 8;
-        System.out.println("The " + n + "th Fibonacci number is: " + fib(n));
-    }
-    
 }
-
-
