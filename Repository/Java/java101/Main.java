c4444c6b4841f4d8a3dccb66e4ae8446f81677b7
Yue Hu Zheng <yuehuz0527@gmail.com>
2024-11-21 16:06:47 -0500

    added main files and readme

--- Repository/Java/java101/Main.java
+++ Repository/Java/java101/Main.java
@@ -2,7 +2,8 @@ package java101;
 
 public class Main{
     public static void main(String[] args) {
-        int [] arr = {2, 3, 8, 9, 0, 3, 0, 4};
-        System.out.println(PracticeProblems.countOccurrences(arr, 0));
+       
     }
 }
+
+
---
+++ Repository/Java/java102/README.md
@@ -0,0 +1,1 @@
+[keep learning Java, using java102.md](https://github.com/SciBorgs/SciGuides/blob/main/projects/intro-to-programming/Java102.md)
